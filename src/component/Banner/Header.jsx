4f9ee4a858852d09893f4a099d71a83aa8915cd9
hiwi-dci-student <hiwot.abdi@dci.education>
2023-04-02 15:57:22 +0200

    responsive header

--- src/component/Banner/Header.jsx
+++ src/component/Banner/Header.jsx
@@ -1,70 +1,153 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import styled from "styled-components";
-import {TbHexagonLetterH} from 'react-icons/tb'
+import { TbHexagonLetterH } from "react-icons/tb";
 
 function Header() {
-  return <Container>
-    <Logo>
-        <span className="green"><TbHexagonLetterH/></span>
+  const [bar, setBar] = useState(false);
+  return (
+    <Container bar={bar}>
+      <Logo>
+        <span className="green">
+          <TbHexagonLetterH />
+        </span>
         <h1>Portfolio</h1>
-    </Logo>
-    <Nav>
-        <span><a href="#">Home</a> </span>
-        <span><a href="#">Services</a> </span>
-        <span><a href="#">Projects</a> </span>
-        <span><a href="#">Testimonials</a> </span>
-        <span><a href="#">Portfolio</a> </span>
-        
-    </Nav>
-    </Container>;
+      </Logo>
+      <Nav bar={bar}>
+        <span>
+          <a href="#">Home</a>{" "}
+        </span>
+        <span>
+          <a href="#">Services</a>{" "}
+        </span>
+        <span>
+          <a href="#">Projects</a>{" "}
+        </span>
+        <span>
+          <a href="#">Testimonials</a>{" "}
+        </span>
+        <span>
+          <a href="#">Portfolio</a>{" "}
+        </span>
+      </Nav>
+      <div onClick={() => setBar(!bar)} className="bars">
+        <div className="bar"></div>
+      </div>
+    </Container>
+  );
 }
 
 export default Header;
 
-const Container = styled.div`   display: flex;
-align-items: center;
-justify-content: space-between;
-max-width: 1280px;
-width: 80%;
-margin: 0 auto;
-padding: 1.5rem 0;`;
-const Logo = styled.div`    display: flex;
-align-items: center;
-gap: 0.5rem;
-span{
-    font-size: 1.8rem;
-   
-}
-
-h1{
-    font-weight: 600;
-    font-size: 1.2rem;
-}`;
-const Nav = styled.div`span {
-    margin-left: 1rem;
-    {
-        color: #fff;
-        text-decoration: none;
-        font-weight: 400;
-        position: relative;
-        :before {
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1280px;
+  width: 80%;
+  margin: 0 auto;
+  padding: 1.5rem 0;
+  @media (max-width: 840px) {
+    width: 90%;
+  }
+  .bars {
+    display: none;
+  }
+  @media (max-width: 640px) {
+    .bars {
+      width: 40px;
+      height: 40px;
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 0.5rem;
+      z-index: 100;
+      .bar {
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        background-color: ${(props) => (props.bar ? "transparent" : "#fff")};
+        transition: all 400ms ease-in-out;
+        :before,
+        :after {
           content: "";
-          position: absolute;
-          left: 0;
-          right: 0;
-          bottom: -5px;
+          width: 100%;
           height: 2px;
           background-color: #fff;
-          transform: scale(0);
-          transform-origin: right;
-          transition: transform 400ms ease-in-out;
+          position: absolute;
         }
-        :hover:before {
-          transform: scale(1);
-          transform-origin: left;
+
+        :before {
+          transform: ${(props) =>
+            props.bar ? "rotate(45deg)" : "translateY(10px)"};
+          transition: all 400ms ease-in-out;
         }
-        :hover {
-          opacity: 0.7;
+
+        :after {
+          transform: ${(props) =>
+            props.bar ? "rotate(-45deg)" : "translateY(-10px)"};
+          transition: all 400ms ease-in-out;
         }
       }
-    }`;
+    }
+  }
+`;
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  span {
+    font-size: 1.8rem;
+  }
+
+  h1 {
+    font-weight: 600;
+    font-size: 1.2rem;
+  }
+`;
+const Nav = styled.div`
+  @media (max-width: 640px) {
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    background-color: #01be96;
+    inset: 0;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    gap: 2rem;
+    font-weight: 700;
+    height: ${(props) => (props.bar ? "100vh" : 0)};
+    transition: height 400ms ease-in-out;
+    overflow: hidden;
+    z-index: 100;
+  }
+  span {
+    margin-left: 1rem;
+     {
+      color: #fff;
+      text-decoration: none;
+      font-weight: 400;
+      position: relative;
+      :before {
+        content: "";
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -5px;
+        height: 2px;
+        background-color: #fff;
+        transform: scale(0);
+        transform-origin: right;
+        transition: transform 400ms ease-in-out;
+      }
+      :hover:before {
+        transform: scale(1);
+        transform-origin: left;
+      }
+      :hover {
+        opacity: 0.7;
+      }
+    }
+  }
+`;
